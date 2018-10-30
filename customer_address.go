30f238054e002179bf9680ea7bf30bffc2c3bfc9
Gord Currie <gord.currie@boldcommerce.com>
2018-10-30 00:03:27 -0500

    Added omitempty tag for CustomerAddress properties
    - was causing unexpected behaviour with partial updates

--- customer_address.go
+++ customer_address.go
@@ -25,21 +25,21 @@ type CustomerAddressServiceOp struct {
 type CustomerAddress struct {
 	ID           int    `json:"id,omitempty"`
 	CustomerID   int    `json:"customer_id,omitempty"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	Company      string `json:"company"`
-	Address1     string `json:"address1"`
-	Address2     string `json:"address2"`
-	City         string `json:"city"`
-	Province     string `json:"province"`
-	Country      string `json:"country"`
-	Zip          string `json:"zip"`
-	Phone        string `json:"phone"`
-	Name         string `json:"name"`
-	ProvinceCode string `json:"province_code"`
-	CountryCode  string `json:"country_code"`
-	CountryName  string `json:"country_name"`
-	Default      bool   `json:"default"`
+	FirstName    string `json:"first_name,omitempty"`
+	LastName     string `json:"last_name,omitempty"`
+	Company      string `json:"company,omitempty"`
+	Address1     string `json:"address1,omitempty"`
+	Address2     string `json:"address2,omitempty"`
+	City         string `json:"city,omitempty"`
+	Province     string `json:"province,omitempty"`
+	Country      string `json:"country,omitempty"`
+	Zip          string `json:"zip,omitempty"`
+	Phone        string `json:"phone,omitempty"`
+	Name         string `json:"name,omitempty"`
+	ProvinceCode string `json:"province_code,omitempty"`
+	CountryCode  string `json:"country_code,omitempty"`
+	CountryName  string `json:"country_name,omitempty"`
+	Default      bool   `json:"default,omitempty"`
 }
 
 // CustomerAddressResoruce represents the result from the addresses/X.json endpoint
